6a4a7ea1a99714d79ccbc374a7098562492dcbc4
alexqin@intv.com.cn <alexqin@intv.com.cn>
2018-02-02 22:12:44 +0800

    稍微修改一下逻辑，并引入了swiper组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,40 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    gifViewSRC: "",
-    containerBackgroundColor: '#fdacad'
+    containerBackgroundColors: [
+      '#fdacad',
+      '#81C7D4'
+    ],
+    characters: [
+      '/assets/cats/red-cat.gif',
+      '/assets/cats/blue-cat.gif'
+    ],
+    currentIndex: 0
   },
-  onLoad: function (options) {
-    // Do some initialize when page load.
+  onSliderChange: function(e) {
     this.setData({
-      gifViewSRC: "../../assets/lollipup_550px_by_x_squishystar_x-d7kobuy.gif"
+      currentIndex: e.detail.current
     })
+    this.changeAnimation()
   },
   changeAnimation: function() {
-    if (this.data.gifViewSRC == "../../assets/lollipup_550px_by_x_squishystar_x-d7kobuy.gif"){
-      this.setData({gifViewSRC: "../../assets/curious_commission_550px_by_x_squishystar_x-d7kl2jj.gif"});
-      this.setData({ containerBackgroundColor: "#81C7D4"});
-      wx.setNavigationBarColor({
-        frontColor: '#ffffff',
-        backgroundColor: '#81C7D4',
-        animation: {
-          duration: 500,
-          timingFunc: 'easeInOut'
-        }
-      })
-    }
-    else{
-      this.setData({ gifViewSRC: "../../assets/lollipup_550px_by_x_squishystar_x-d7kobuy.gif"})
-      this.setData({ containerBackgroundColor: "#fdacad" });
-      wx.setNavigationBarColor({
-        frontColor: '#ffffff',
-        backgroundColor: '#fdacad',
-        animation: {
-          duration: 500,
-          timingFunc: 'easeInOut'
-        }
-      })
-    }
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: this.data.containerBackgroundColors[this.data.currentIndex],
+      animation: {
+        duration: 500,
+        timingFunc: 'easeInOut'
+      }
+    })
   },
   //事件处理函数
   startRecord: function() {
